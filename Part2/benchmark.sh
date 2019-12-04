d2ad5a67ac08a47b9af9526f95ad05606640e3ff
Yuxin Yang <yangyuxin60@live.com>
2019-12-04 11:52:20 -0500

    Update benchmark.sh

--- Part2/benchmark.sh
+++ Part2/benchmark.sh
@@ -1,8 +1,8 @@
 sudo apt-get update
 
-sudo apt-get install build-essential
+sudo apt-get install -y build-essential
 
-sudo apt install git-all
+sudo apt install -y git-all
 
 git clone https://github.com/arihant15/Performance-Evaluation-Benchmark.git
 
